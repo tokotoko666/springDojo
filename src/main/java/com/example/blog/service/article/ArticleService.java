57b6f330e5b95e68d3e2beecb2f1c690f0aaf298
tokotoko666 <cappuccino969@gmail.com>
2025-12-04 11:24:23 +0900

    01-18_3層アーキテクチャを導入する

---
+++ src/main/java/com/example/blog/service/article/ArticleService.java
@@ -0,0 +1,17 @@
+package com.example.blog.service.article;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class ArticleService {
+
+    public Optional<ArticleEntity> findById(long id) {
+        return Optional.of(new ArticleEntity(
+                id,
+                "title",
+                "content",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        ));
+    }
+}
--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,22 +1,23 @@
 package com.example.blog.web.controller.article;
 
+import com.example.blog.service.article.ArticleService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 
 @RestController
 public class ArticleRestController {
 
+    private final ArticleService articleService = new ArticleService();
+
     @GetMapping("/articles/{id}")
     public ArticleDTO showArticle(@PathVariable("id") long id) {
-        return new ArticleDTO(
-                id,
-                "This is title: id" + id,
-                "This is content",
-                LocalDateTime.now(),
-                LocalDateTime.now()
-        );
+        return articleService.findById(id)
+                .map(ArticleDTO::from)
+                .orElseThrow((() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
 }
