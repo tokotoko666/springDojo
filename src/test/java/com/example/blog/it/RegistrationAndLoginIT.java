5c0a7c97c51f093a124beff78bd9bf7cb3a80460
tokotoko666 <cappuccino969@gmail.com>
2025-12-23 17:58:21 +0900

    03-25_ログイン失敗のテスト実装：ユーザー名が存在しない

--- src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
+++ src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
@@ -52,6 +52,8 @@ public class RegistrationAndLoginIT {
         loginFailure_DifferentToken(xsrfToken);
 
         //ユーザー名が存在しない
+        loginFailure_GivenUsernameDoesNotExistInDatabase(xsrfToken);
+
         //パスワードがデータベースに保存されているパスワードと違う
         //ログイン成功
         loginSuccess(xsrfToken);
@@ -197,5 +199,28 @@ public class RegistrationAndLoginIT {
 
         // ## Assert ##
         responseSpec.expectStatus().isForbidden();
+    }
+
+    private void loginFailure_GivenUsernameDoesNotExistInDatabase(String xsrfToken) {
+        // ## Arrange ##
+        var bodyJson = String.format("""
+                {
+                  "username": "%s",
+                  "password": "%s"
+                }
+                """, TEST_USERNAME + "_invalid", TEST_PASSWORD);
+
+        // ## Act ##
+        var responseSpec = webTestClient
+                .post().uri("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .cookie("XSRF-TOKEN", xsrfToken)
+                .cookie("JSESSIONID", DUMMY_SESSION_ID)
+                .header("X-XSRF-TOKEN", xsrfToken)
+                .bodyValue(bodyJson)
+                .exchange();
+
+        // ## Assert ##
+        responseSpec.expectStatus().isUnauthorized();
     }
 }
