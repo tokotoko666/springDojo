751ccff1b41ac3c5b5038ca09f3aeeecaab332b2
tokotoko666 <cappuccino969@gmail.com>
2025-12-23 18:02:31 +0900

    03-26_ログイン失敗のテスト実装：パスワード間違い

--- src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
+++ src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
@@ -55,6 +55,8 @@ public class RegistrationAndLoginIT {
         loginFailure_GivenUsernameDoesNotExistInDatabase(xsrfToken);
 
         //パスワードがデータベースに保存されているパスワードと違う
+        loginFailure_GivenPasswordIsNotSameAsPasswordInDatabase(xsrfToken);
+
         //ログイン成功
         loginSuccess(xsrfToken);
         //ユーザー名がデータベースに存在する
@@ -209,6 +211,29 @@ public class RegistrationAndLoginIT {
                   "password": "%s"
                 }
                 """, TEST_USERNAME + "_invalid", TEST_PASSWORD);
+
+        // ## Act ##
+        var responseSpec = webTestClient
+                .post().uri("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .cookie("XSRF-TOKEN", xsrfToken)
+                .cookie("JSESSIONID", DUMMY_SESSION_ID)
+                .header("X-XSRF-TOKEN", xsrfToken)
+                .bodyValue(bodyJson)
+                .exchange();
+
+        // ## Assert ##
+        responseSpec.expectStatus().isUnauthorized();
+    }
+
+    private void loginFailure_GivenPasswordIsNotSameAsPasswordInDatabase(String xsrfToken) {
+        // ## Arrange ##
+        var bodyJson = String.format("""
+                {
+                  "username": "%s",
+                  "password": "%s"
+                }
+                """, TEST_USERNAME, TEST_PASSWORD + "_invalid");
 
         // ## Act ##
         var responseSpec = webTestClient
