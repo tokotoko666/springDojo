0d4382ef6560b1946ac2c5bafc439665796f5945
tokotoko666 <cappuccino969@gmail.com>
2025-12-24 16:02:31 +0900

    04-13_Repository のテスト：selectById が Optional-ArticleEntity- を返す場合

--- src/test/java/com/example/blog/repository/article/ArticleRepositoryTest.java
+++ src/test/java/com/example/blog/repository/article/ArticleRepositoryTest.java
@@ -1,9 +1,11 @@
 package com.example.blog.repository.article;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,5 +20,28 @@ class ArticleRepositoryTest {
     @Test
     public void test() {
         assertThat(cut).isNotNull();
+    }
+
+    @Test
+    @DisplayName("selectById: 指定されたIDの記事が存在するとき、ArticleEntity を返す")
+    @Sql(statements = """
+            INSERT INTO articles(id, title, body, created_at, updated_at)
+            VALUES(999, 'title_999', 'body_999', '2010-10-01 00:00:00', '2010-11-01 00:00:00');
+            """)
+    public void selectById_returnArticleEntity() {
+        // ## Arrange ##
+
+        // ## Act ##
+        var actual = cut.selectById(999L);
+
+        // ## Assert ##
+        assertThat(actual).isPresent()
+                .hasValueSatisfying(articleEntity -> {
+                    assertThat(articleEntity.id()).isEqualTo(999L);
+                    assertThat(articleEntity.title()).isEqualTo("title_999");
+                    assertThat(articleEntity.content()).isEqualTo("body_999");
+                    assertThat(articleEntity.createdAt()).isEqualTo("2010-10-01T00:00:00");
+                    assertThat(articleEntity.updatedAt()).isEqualTo("2010-11-01T00:00:00");
+                });
     }
 }
