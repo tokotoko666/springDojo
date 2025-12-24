0c73f85328fc5b4c603f44a2c7be3819d271f256
tokotoko666 <cappuccino969@gmail.com>
2025-12-24 17:55:11 +0900

    04-21_Serivce のテスト：Mockito の練習

--- src/test/java/com/example/blog/service/article/ArticleServiceMockTest.java
+++ src/test/java/com/example/blog/service/article/ArticleServiceMockTest.java
@@ -7,7 +7,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ArticleServiceMockTest {
@@ -20,5 +23,18 @@ class ArticleServiceMockTest {
     @Test
     public void cut() {
         assertThat(cut).isNotNull();
+    }
+
+    @Test
+    public void mockPractice() {
+        when(cut.findById(999L)).thenReturn(Optional.of(
+                new ArticleEntity(999, "", "", null, null)
+        ));
+
+        assertThat(articleRepository.selectById(999L)).isPresent()
+                .hasValueSatisfying(articleEntity -> {
+                    assertThat(articleEntity.id()).isEqualTo(999L);
+                });
+        assertThat(articleRepository.selectById(111L)).isEmpty();
     }
 }
