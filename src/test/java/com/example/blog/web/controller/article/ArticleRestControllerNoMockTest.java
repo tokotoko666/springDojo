f6d26fbd740951ee2a9db4e23670cd4fbb913e5a
tokotoko666 <cappuccino969@gmail.com>
2025-12-25 14:13:59 +0900

    04-32_Controller のテスト（モックなし）：200 OK

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerNoMockTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerNoMockTest.java
@@ -1,15 +1,22 @@
 package com.example.blog.web.controller.article;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class ArticleRestControllerNoMockTest {
 
     @Autowired
@@ -18,5 +25,27 @@ class ArticleRestControllerNoMockTest {
     @Test
     public void mockMvc() {
         assertThat(mockMvc).isNotNull();
+    }
+
+    @Test
+    @DisplayName("GET /articles/{id}: 指定されたIDの記事が存在するとき、200OK で記事データを返す")
+    @Sql(statements = """
+            INSERT INTO articles(id, title, body, created_at, updated_at)
+            VALUES(999, 'title_999', 'body_999', '2022-01-02 03:04:05', '2023-01-02 03:04:05');
+            """)
+    public void getArticle_return200() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        var actual = mockMvc.perform(get("/articles/{id}", 999));
+
+        // ## Assert ##
+        actual.andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(999L))
+                .andExpect(jsonPath("$.title").value("title_999"))
+                .andExpect(jsonPath("$.content").value("body_999"))
+                .andExpect(jsonPath("$.createdAt").value("2022-01-02T03:04:05"))
+                .andExpect(jsonPath("$.updatedAt").value("2023-01-02T03:04:05"))
+        ;
     }
 }
