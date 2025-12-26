19c25e79d6529d889145359602fb4a252bedb799
tokotoko666 <cappuccino969@gmail.com>
2025-12-26 15:11:29 +0900

    05-21_GET /csrf-cookie のテスト：レスポンスが 204 のケース

---
+++ src/test/java/com/example/blog/web/controller/CSRFCookieRestControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.blog.web.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CSRFCookieRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("GET /csrf-cookie: 204 を返し、Set-Cookie ヘッダに XSRF-TOKEN が含まれている")
+    void return204() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        var result = mockMvc.perform(get("/csrf-cookie"));
+
+        // ## Assert ##
+        result.andExpect(status().isNoContent()).andExpect(header().string("Set-Cookie", containsString("XSRF-TOKEN")));
+    }
+}
