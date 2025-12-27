488e85424ea5e83185a29336a25da2d4f489a5a4
tokotoko666 <cappuccino969@gmail.com>
2025-12-27 22:01:24 +0900

    06-17_UserRepository の修正_selectByUsername のテストを書く

--- src/test/java/com/example/blog/repository/user/UserRepositoryTest.java
+++ src/test/java/com/example/blog/repository/user/UserRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.example.blog.repository.user;
 
 import com.example.blog.config.MybatisDefaultDataSourceTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,4 +19,24 @@ class UserRepositoryTest {
         assertThat(cut).isNotNull();
     }
 
+    @Test
+    @DisplayName("selectByUsername: 指定されたユーザー名のユーザーが存在するとき、Optional<UserEntity>を返す")
+    @Sql(statements = {
+            "INSERT INTO users(id, username, password, enabled)VALUES (999, 'test_user_1', 'test_user_1_pass', true);",
+            "INSERT INTO users(id, username, password, enabled) VALUES(998, 'test_user_2', 'test_user_2_pass', true);"
+    })
+    void selectByUsername_success() {
+        // ## Arrange ##
+
+        // ## Act ##
+        var actual = cut.selectByUsername("test_user_1");
+
+        // ## Assert ##
+        assertThat(actual).hasValueSatisfying(actualEntity -> {
+            // TODO id も比べたい
+            assertThat(actualEntity.username()).isEqualTo("test_user_1");
+            assertThat(actualEntity.password()).isEqualTo("test_user_1_pass");
+            assertThat(actualEntity.enabled()).isTrue();
+        });
+    }
 }
