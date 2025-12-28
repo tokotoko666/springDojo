d7c97c5c342ba7e5706d32e6846acb45a109ec5e
tokotoko666 <cappuccino969@gmail.com>
2025-12-28 21:28:45 +0900

    06-41_ユーザー登録：バリデーション_validator の初期化と後始末を共通化する

--- src/test/java/com/example/blog/web/controller/user/UserFormTest.java
+++ src/test/java/com/example/blog/web/controller/user/UserFormTest.java
@@ -2,6 +2,10 @@ package com.example.blog.web.controller.user;
 
 import com.example.blog.model.UserForm;
 import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +13,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class UserFormTest {
 
+    private ValidatorFactory validatorFactory;
+    private Validator validator;
+
+    @BeforeEach
+    void beforeEach() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @AfterEach
+    void afterEach() {
+        validatorFactory.close();
+    }
+
     @Test
     @DisplayName("username のバリデーション：成功")
     void username_success() {
         // ## Arrange ##
-        var validatorFactory = Validation.buildDefaultValidatorFactory();
-        var validator = validatorFactory.getValidator();
         var userForm = new UserForm("username00", "password00");
 
         // ## Act ##
@@ -28,8 +44,6 @@ class UserFormTest {
     @DisplayName("username のバリデーション：失敗")
     void username_failure() {
         // ## Arrange ##
-        var validatorFactory = Validation.buildDefaultValidatorFactory();
-        var validator = validatorFactory.getValidator();
         var userForm = new UserForm(null, "password00");
 
         // ## Act ##
