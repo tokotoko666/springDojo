42a0f4847e8cf10f66d015331ad31b4519c212a6
tokotoko666 <cappuccino969@gmail.com>
2025-12-28 19:13:19 +0900

    06-34_ユーザー登録_UserController のテスト_UsersApi を implements する

--- src/main/java/com/example/blog/web/controller/user/UserRestController.java
+++ src/main/java/com/example/blog/web/controller/user/UserRestController.java
@@ -1,28 +1,31 @@
 package com.example.blog.web.controller.user;
 
+import com.example.blog.api.UsersApi;
+import com.example.blog.model.UserDTO;
+import com.example.blog.model.UserForm;
 import com.example.blog.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
-public class UserRestController {
+public class UserRestController implements UsersApi {
 
     private final UserService userService;
 
-    @GetMapping("/me")
+    @GetMapping("/users/me")
     public ResponseEntity<String> me(Principal principal) {
         return ResponseEntity.ok(principal.getName());
     }
 
-    @PostMapping
-    public ResponseEntity<Void> create(@RequestBody UserForm userform) {
-        var newUser = userService.register(userform.username(), userform.password());
+    @Override
+    public ResponseEntity<UserDTO> createUser(UserForm userForm) {
+        var newUser = userService.register(userForm.getUsername(), userForm.getPassword());
         var location = UriComponentsBuilder.fromPath("/users/{id}")
                 .buildAndExpand(newUser.getId())
                 .toUri();
