6d0582d44c3cb5784aac51bf12b8dcffd07ec00d
tokotoko666 <cappuccino969@gmail.com>
2025-12-28 21:14:41 +0900

    06-38_ユーザー登録：バリデーション_Form クラスだけでテストをするように書き換える

---
+++ src/test/java/com/example/blog/web/controller/user/UserFormTest.java
@@ -0,0 +1,26 @@
+package com.example.blog.web.controller.user;
+
+import com.example.blog.model.UserForm;
+import jakarta.validation.Validation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserFormTest {
+
+    @Test
+    @DisplayName("username のバリデーション：成功")
+    void username_success() {
+        // ## Arrange ##
+        var validatorFactory = Validation.buildDefaultValidatorFactory();
+        var validator = validatorFactory.getValidator();
+        var userForm = new UserForm("username00", "password00");
+
+        // ## Act ##
+        var actual = validator.validate(userForm);
+
+        // ## Assert ##
+        assertThat(actual).isEmpty();
+    }
+}
