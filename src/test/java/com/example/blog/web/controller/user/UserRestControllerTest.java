fdb45020867e0944bdfe139ce6feee8cb3a2c504
tokotoko666 <cappuccino969@gmail.com>
2025-12-29 21:46:40 +0900

    06-53_ユーザー登録：バリデーションエラー時のレスポンスボディを確認する UT を書く

--- src/test/java/com/example/blog/web/controller/user/UserRestControllerTest.java
+++ src/test/java/com/example/blog/web/controller/user/UserRestControllerTest.java
@@ -140,6 +140,11 @@ class UserRestControllerTest {
         // ## Assert ##
         actual
                 .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.title").value("Bad Request"))
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.detail").value("Invalid request content."))
+                .andExpect(jsonPath("$.type").value("about:blank"))
+                .andExpect(jsonPath("$.instance").isEmpty())
                 .andDo(print());
     }
 }
