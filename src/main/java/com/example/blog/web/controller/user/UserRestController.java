78226bb160e8e6e464bb70751a02e640d37a779e
tokotoko666 <cappuccino969@gmail.com>
2025-12-29 22:44:49 +0900

    06-59_ユーザー登録：バリデーション_エラーコードをメッセージに変換する

--- src/main/java/com/example/blog/web/controller/user/UserRestController.java
+++ src/main/java/com/example/blog/web/controller/user/UserRestController.java
@@ -8,6 +8,8 @@ import com.example.blog.model.UserForm;
 import com.example.blog.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -25,6 +27,7 @@ public class UserRestController implements UsersApi {
 
     private final UserService userService;
     private final DuplicateUsernameValidator duplicateUsernameValidator;
+    private final MessageSource messageSource;
 
     @InitBinder
     public void initBinder(DataBinder dataBinder) {
@@ -55,11 +58,12 @@ public class UserRestController implements UsersApi {
         var body = new BadRequest();
         BeanUtils.copyProperties(e.getBody(), body);
 
+        var locale = LocaleContextHolder.getLocale();
         var errorDetailList = e.getBindingResult().getFieldErrors()
                 .stream()
                 .map(fieldError -> {
                     var pointer = "#/" + fieldError.getField();
-                    var detail = fieldError.getCode();
+                    var detail = messageSource.getMessage(fieldError, locale);
                     var errorDetail = new ErrorDetail();
                     errorDetail.setPointer(pointer);
                     errorDetail.setDetail(detail);
