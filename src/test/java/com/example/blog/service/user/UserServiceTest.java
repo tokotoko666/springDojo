874af2265736eb86dc682244b3bdc44c153e39f9
tokotoko666 <cappuccino969@gmail.com>
2025-12-29 20:23:45 +0900

    06-49_ユーザー登録：バリデーション_UserSerivce-existsUsername メソッドのUT_存在する場合

--- src/test/java/com/example/blog/service/user/UserServiceTest.java
+++ src/test/java/com/example/blog/service/user/UserServiceTest.java
@@ -50,4 +50,20 @@ class UserServiceTest {
                     .isTrue();
         });
     }
+
+    @Test
+    @DisplayName("existsUsername: ユーザー名がすでに登録済みのとき true")
+    void existsUsername_returnTrue() {
+        // ## Arrange ##
+        var username = "test_username";
+        var alreadyExistUser = new UserEntity(null, username, "test_password", true);
+        userRepository.insert(alreadyExistUser);
+
+        // ## Act ##
+        var actual = cut.existsUsername(username);
+
+        // ## Assert ##
+        assertThat(actual).isTrue();
+    }
+
 }
