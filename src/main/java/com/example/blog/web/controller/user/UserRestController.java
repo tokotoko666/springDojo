fce648a8790d201473818253010341273169c938
tokotoko666 <cappuccino969@gmail.com>
2025-12-29 21:58:52 +0900

    06-54_ユーザー登録：バリデーションエラー時のレスポンスボディを実装する

--- src/main/java/com/example/blog/web/controller/user/UserRestController.java
+++ src/main/java/com/example/blog/web/controller/user/UserRestController.java
@@ -1,12 +1,15 @@
 package com.example.blog.web.controller.user;
 
 import com.example.blog.api.UsersApi;
+import com.example.blog.model.BadRequest;
 import com.example.blog.model.UserDTO;
 import com.example.blog.model.UserForm;
 import com.example.blog.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.DataBinder;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +46,12 @@ public class UserRestController implements UsersApi {
         return ResponseEntity
                 .created(location)
                 .body(dto);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BadRequest> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var body = new BadRequest();
+        // TODO
+        return ResponseEntity.badRequest().build();
     }
 }
