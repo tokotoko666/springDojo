11e890ee9a554de623980ec3046cb2c4e51ea534
tokotoko666 <cappuccino969@gmail.com>
2025-12-30 21:54:28 +0900

    06-74_ログイン_MockMvcと使ったテストの準備をする

---
+++ src/test/java/com/example/blog/web/controller/LoginLogoutTest.java
@@ -0,0 +1,24 @@
+package com.example.blog.web.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LoginLogoutTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("test description")
+    void mockMvc() {
+        assertThat(mockMvc).isNotNull();
+    }
+}
