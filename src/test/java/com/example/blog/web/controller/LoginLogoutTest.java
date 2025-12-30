d409b681a6e4a63e74bec0a515b8f986f301e5ac
tokotoko666 <cappuccino969@gmail.com>
2025-12-30 22:24:13 +0900

    06-80_ログアウト_テストを書く

--- src/test/java/com/example/blog/web/controller/LoginLogoutTest.java
+++ src/test/java/com/example/blog/web/controller/LoginLogoutTest.java
@@ -106,4 +106,21 @@ class LoginLogoutTest {
         actual.andExpect(status().isUnauthorized())
                 .andExpect(unauthenticated());
     }
+
+    @Test
+    @DisplayName("POST /logout: ログアウト成功")
+    void logout_success() throws Exception {
+
+        // ## Arrange ##
+
+        // ## Act ##
+        var actual = mockMvc.perform(
+                post("/logout")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // ## Assert ##
+        actual.andExpect(status().isOk());
+    }
 }
