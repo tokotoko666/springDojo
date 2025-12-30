2195659e52aca5712d2a3b4eee0e5913da37a4f5
tokotoko666 <cappuccino969@gmail.com>
2025-12-30 18:23:31 +0900

    06-68_ユーザー登録：500 Internal Server Error のレスポンスボディのUTにアサートを追加する

--- src/test/java/com/example/blog/web/controller/user/UserRestController500InternalServerErrorTest.java
+++ src/test/java/com/example/blog/web/controller/user/UserRestController500InternalServerErrorTest.java
@@ -11,10 +11,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.aMapWithSize;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -60,6 +61,14 @@ public class UserRestController500InternalServerErrorTest {
         );
 
         // ## Assert ##
-        actual.andExpect(status().isInternalServerError());
+        actual.andExpect(status().isInternalServerError())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andExpect(jsonPath("$.title").value("Internal Server Error"))
+                .andExpect(jsonPath("$.status").value(500))
+                .andExpect(jsonPath("$.detail").isEmpty())
+                .andExpect(jsonPath("$.type").value("about:blank"))
+                .andExpect(jsonPath("$.instance").isEmpty())
+                .andExpect(jsonPath("$", aMapWithSize(5)))
+        ;
     }
 }
