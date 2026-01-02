fe5baa8c6fbbb698aa67ab1ebc667f823224b8be
tokotoko666 <cappuccino969@gmail.com>
2026-01-02 20:46:09 +0900

    07-08_POST :articles：201 Created を返すようにモック実装をする

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,15 +1,19 @@
 package com.example.blog.web.controller.article;
 
+import com.example.blog.api.ArticlesApi;
 import com.example.blog.service.article.ArticleService;
 import com.example.blog.web.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 @RequiredArgsConstructor
-public class ArticleRestController {
+public class ArticleRestController implements ArticlesApi {
 
     private final ArticleService articleService;
 
@@ -18,5 +22,10 @@ public class ArticleRestController {
         return articleService.findById(id)
                 .map(ArticleDTO::from)
                 .orElseThrow((ResourceNotFoundException::new));
+    }
+
+    @Override
+    public ResponseEntity<Void> createArticle() {
+        return ResponseEntity.created(URI.create("about:blank")).build();
     }
 }
