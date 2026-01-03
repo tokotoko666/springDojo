9ad8551d31a3c9d8be8378930091529bc78919c0
tokotoko666 <cappuccino969@gmail.com>
2026-01-03 17:06:46 +0900

    07-17_POST :articles：201 Created のテストを詳細に書く

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.matchesPattern;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -36,16 +37,38 @@ class ArticleRestControllerTest {
     @DisplayName("POST /articles: 新規記事の作成に成功する")
     void createArticles_201Created() throws Exception {
         // ## Arrange ##
+        var expectedUsername = "test_user";
+        var expectedTitle = "test_title";
+        var expectedBody = "test_body";
 
+        var bodyJson = """
+                {
+                  "title": "%s",
+                  "body": "%s"
+                }
+                """.formatted(expectedTitle, expectedBody);
         // ## Act ##
         var actual = mockMvc.perform(
                 post("/articles")
                         .with(csrf())
-                        .with(user("user1"))
+                        .with(user(expectedUsername))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(bodyJson)
         );
 
         // ## Assert ##
-        actual.andExpect(status().isCreated());
+
+        actual.andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(header().string("Location", matchesPattern("/articles/\\d+")))
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.title").value(expectedTitle))
+                .andExpect(jsonPath("$.body").value(expectedBody))
+                .andExpect(jsonPath("$.author.id").isNumber())
+                .andExpect(jsonPath("$.author.name").value(expectedUsername))
+                .andExpect(jsonPath("$.createdAt").isNotEmpty())
+                .andExpect(jsonPath("$.updatedAt").isNotEmpty())
+        ;
     }
 
     @Test
