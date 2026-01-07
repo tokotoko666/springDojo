15b12f1037389d5282a36a55bea7a886014adacc
tokotoko666 <cappuccino969@gmail.com>
2026-01-07 22:10:50 +0900

    07-24_POST :articles：ログインユーザー情報にユーザーIDを保持するように変更する

---
+++ src/main/java/com/example/blog/security/LoggedInUser.java
@@ -0,0 +1,17 @@
+package com.example.blog.security;
+
+import lombok.Getter;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+@Getter
+public class LoggedInUser extends User {
+
+    private final long userId;
+
+    public LoggedInUser(long userId, String username, String password, boolean enabled) {
+        super(username, password, enabled, true, true, true, List.of());
+        this.userId = userId;
+    }
+}
