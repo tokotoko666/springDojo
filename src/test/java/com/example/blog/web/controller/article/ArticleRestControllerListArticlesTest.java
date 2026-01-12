154bfd2fd8e9e4dad82eb8fedc63997d9ec243a7
tokotoko666 <cappuccino969@gmail.com>
2026-01-12 21:57:44 +0900

    07-50_GET :articles：200 OK のテストを書きはじめる

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerListArticlesTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerListArticlesTest.java
@@ -1,13 +1,18 @@
 package com.example.blog.web.controller.article;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,6 +25,18 @@ class ArticleRestControllerListArticlesTest {
     @Test
     void setUp() {
         assertThat(mockMvc).isNotNull();
+    }
+
+    @Test
+    @DisplayName("GET /articles: 記事の一覧を取得できる")
+    void listArticles_success() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        var actual = mockMvc.perform(get("/articles").contentType(MediaType.APPLICATION_JSON));
+
+        // ## Assert ##
+        actual.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
 }
