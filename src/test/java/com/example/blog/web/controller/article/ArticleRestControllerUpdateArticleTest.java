995f097fae62cf6d70dd9bc4739f4112a210e585
tokotoko666 <cappuccino969@gmail.com>
2026-01-17 22:19:39 +0900

    08-14_PUT :articles:-id-：404 Not Found のテストを書く

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerUpdateArticleTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerUpdateArticleTest.java
@@ -77,6 +77,40 @@ class ArticleRestControllerUpdateArticleTest {
         ;
     }
 
+    @Test
+    @DisplayName("PUT /articles/{articleId}: 指定されたIDの記事が存在しないとき、404を返す")
+    void updateArticles_404OK() throws Exception {
+        // ## Arrange ##
+        var newUser = userService.register("test_username", "test_password");
+        var expectedUser = new LoggedInUser(newUser.getId(), newUser.getUsername(), newUser.getPassword(), newUser.isEnabled());
+        var invalidArticleId = 0;
+        var bodyJson = """
+                {
+                  "title": "test_title_updated",
+                  "body": "test_body_updated"
+                }
+                """;
+
+        // ## Act ##
+        var actual = mockMvc.perform(
+                put("/articles/{articleId}", invalidArticleId)
+                        .with(csrf())
+                        .with(user(expectedUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(bodyJson)
+        );
+
+        // ## Assert ##
+        actual
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.detail").value("リソースが見つかりません"))
+                .andExpect(jsonPath("$.instance").value("/articles/" + invalidArticleId))
+        ;
+    }
+
 //    @Test
 //    @DisplayName("POST /articles: リクエストの title フィールドがバリデーションNGのとき、400 BadRequest")
 //    void createArticles_400BadRequest() throws Exception {
