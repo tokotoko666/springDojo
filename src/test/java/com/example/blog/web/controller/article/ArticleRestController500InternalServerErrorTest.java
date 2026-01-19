e48a08945b88921cdf06154fbb10c771c62cf347
tokotoko666 <cappuccino969@gmail.com>
2026-01-19 22:24:09 +0900

    08-25_PUT :articles:-id-：500 Internal Server Error のテストを書く

--- src/test/java/com/example/blog/web/controller/article/ArticleRestController500InternalServerErrorTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestController500InternalServerErrorTest.java
@@ -16,8 +16,7 @@ import static org.hamcrest.Matchers.aMapWithSize;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -112,6 +111,44 @@ public class ArticleRestController500InternalServerErrorTest {
         var actual = mockMvc.perform(
                 get("/articles/{articleUd}", articleId)
                         .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // ## Assert ##
+        actual
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andExpect(jsonPath("$.title").value("Internal Server Error"))
+                .andExpect(jsonPath("$.status").value(500))
+                .andExpect(jsonPath("$.detail").isEmpty())
+                .andExpect(jsonPath("$.instance").value("/articles/" + articleId))
+                .andExpect(jsonPath("$", aMapWithSize(4)))
+        ;
+    }
+
+    @Test
+    @DisplayName("PUT /articles/{articleId}: 500 InternalServerError で stacktrace が露出しない")
+    void putArticle_500() throws Exception {
+        // ## Arrange ##
+        var userId = 999L;
+        var articleId = 9999L;
+        var title = "test_title";
+        var body = "test_body";
+        when(articleService.update(userId, articleId, title, body)).thenThrow(RuntimeException.class);
+
+        var bodyJson = """
+                {
+                  "title": "%s",
+                  "body": "%s"
+                }
+                """.formatted(title, body);
+
+        // ## Act ##
+        var actual = mockMvc.perform(
+                put("/articles/{articleId}", articleId)
+                        .with(csrf())
+                        .with(user(new LoggedInUser(userId, "test_username", "test_password", true)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(bodyJson)
         );
 
         // ## Assert ##
