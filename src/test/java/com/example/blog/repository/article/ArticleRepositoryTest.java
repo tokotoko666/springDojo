f806b91e9ac2f05dbcbdbce852b053af258b6caa
tokotoko666 <cappuccino969@gmail.com>
2026-01-21 22:09:51 +0900

    08-30_PUT :articles:-id-：Repository 層のテストを追加する_更新成功

--- src/test/java/com/example/blog/repository/article/ArticleRepositoryTest.java
+++ src/test/java/com/example/blog/repository/article/ArticleRepositoryTest.java
@@ -150,4 +150,48 @@ class ArticleRepositoryTest {
                 .ignoringFields("author.password")
                 .isEqualTo(expectedArticle1);
     }
+
+    @Test
+    @DisplayName("update: 記事の title/body/updated_at を更新できる")
+    void update_success() {
+        // ## Arrange ##
+        var expectedTitle = "test_title_updated";
+        var expectedBody = "test_body_updated";
+        var expectedCreatedAt = TestDateTimeUtil.of(2020, 1, 10, 10, 20, 30);
+        var expectedUpdatedAt = expectedCreatedAt.plusDays(1);
+
+        var expectedUser = new UserEntity(null, "test_username", "test_password", true);
+        userRepository.insert(expectedUser);
+
+        var articleToCreate = new ArticleEntity(
+                null,
+                "test_title",
+                "test_body",
+                expectedUser,
+                expectedCreatedAt,
+                expectedCreatedAt // 記事を新規作成したため、createdAt = updatedAt
+        );
+        cut.insert(articleToCreate);
+
+        var articleToUpdate = new ArticleEntity(
+                articleToCreate.getId(),
+                expectedTitle,
+                expectedBody,
+                articleToCreate.getAuthor(),
+                articleToCreate.getCreatedAt(),
+                expectedUpdatedAt
+        );
+
+        // ## Act ##
+        cut.update(articleToUpdate);
+
+        // ## Assert ##
+        var actual = cut.selectById(articleToCreate.getId());
+        assertThat(actual).hasValueSatisfying(actualArticle -> {
+            assertThat(actualArticle)
+                    .usingRecursiveComparison()
+                    .ignoringFields("author.password")
+                    .isEqualTo(articleToUpdate);
+        });
+    }
 }
