93193be7132b2d9ebb4e9d02c81c636f049fbfdc
tokotoko666 <cappuccino969@gmail.com>
2026-01-22 22:19:49 +0900

    08-39_DELETE :articles:-id-：403 Forbidden ①_編集権限がないリソースに対するリクエストのテストを書く

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerDeleteArticleTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerDeleteArticleTest.java
@@ -108,6 +108,32 @@ class ArticleRestControllerDeleteArticleTest {
     }
 
     @Test
+    @DisplayName("DELETE /articles/{articleId}: 自分が作成した記事以外の記事を削除しようとしたとき 403 を返す")
+    void deleteArticles_403Forbidden_authorId() throws Exception {
+        // ## Arrange ##
+        var otherUser = userService.register("test_username2", "test_password2");
+        var loggedInOtherUser = new LoggedInUser(otherUser.getId(), author.getUsername(), author.getPassword(), author.isEnabled());
+
+        // ## Act ##
+        var actual = mockMvc.perform(
+                delete("/articles/{articleId}", existingArticle.getId())
+                        .with(csrf())
+                        .with(user(loggedInOtherUser))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // ## Assert ##
+        actual
+                .andExpect(status().isForbidden())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andExpect(jsonPath("$.title").value("Forbidden"))
+                .andExpect(jsonPath("$.status").value(403))
+                .andExpect(jsonPath("$.detail").value("リソースへのアクセスが拒否されました"))
+                .andExpect(jsonPath("$.instance").value("/articles/" + existingArticle.getId()))
+        ;
+    }
+
+    @Test
     @DisplayName("PUT /articles/{articleId}: 指定されたIDの記事が存在しないとき、404を返す")
     void updateArticles_404OK() throws Exception {
         // ## Arrange ##
@@ -136,39 +162,6 @@ class ArticleRestControllerDeleteArticleTest {
                 .andExpect(jsonPath("$.status").value(404))
                 .andExpect(jsonPath("$.detail").value("リソースが見つかりません"))
                 .andExpect(jsonPath("$.instance").value("/articles/" + invalidArticleId))
-        ;
-    }
-
-    @Test
-    @DisplayName("PUT /articles/{articleId}: 自分が作成した記事以外の記事を編集しようとしたとき 403 を返す")
-    void updateArticles_403Forbidden_authorId() throws Exception {
-        // ## Arrange ##
-        var otherUser = userService.register("test_username2", "test_password2");
-        var loggedInOtherUser = new LoggedInUser(otherUser.getId(), author.getUsername(), author.getPassword(), author.isEnabled());
-        var bodyJson = """
-                {
-                  "title": "test_title_updated",
-                  "body": "test_body_updated"
-                }
-                """;
-
-        // ## Act ##
-        var actual = mockMvc.perform(
-                put("/articles/{articleId}", existingArticle.getId())
-                        .with(csrf())
-                        .with(user(loggedInOtherUser))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(bodyJson)
-        );
-
-        // ## Assert ##
-        actual
-                .andExpect(status().isForbidden())
-                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
-                .andExpect(jsonPath("$.title").value("Forbidden"))
-                .andExpect(jsonPath("$.status").value(403))
-                .andExpect(jsonPath("$.detail").value("リソースへのアクセスが拒否されました"))
-                .andExpect(jsonPath("$.instance").value("/articles/" + existingArticle.getId()))
         ;
     }
 
