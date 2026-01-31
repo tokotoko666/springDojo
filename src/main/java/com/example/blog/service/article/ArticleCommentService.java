819030462ff00ebc829fa1662e1b4b65881b0fcb
tokotoko666 <cappuccino969@gmail.com>
2026-01-31 20:47:31 +0900

    09-19_POST comment_404 NotFound：実装する

--- src/main/java/com/example/blog/service/article/ArticleCommentService.java
+++ src/main/java/com/example/blog/service/article/ArticleCommentService.java
@@ -1,19 +1,26 @@
 package com.example.blog.service.article;
 
 import com.example.blog.repository.article.ArticleCommentRepository;
+import com.example.blog.repository.article.ArticleRepository;
 import com.example.blog.service.DateTimeService;
+import com.example.blog.service.exception.ResourceNotFoundException;
 import com.example.blog.service.user.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleCommentService {
 
     private final ArticleCommentRepository articleCommentRepository;
+    private final ArticleRepository articleRepository;
     private final DateTimeService dateTimeService;
 
+    @Transactional
     public ArticleCommentEntity create(long userId, long articleId, String body) {
+
+        articleRepository.selectById(articleId).orElseThrow(ResourceNotFoundException::new);
 
         var newComment = new ArticleCommentEntity(
                 null,
