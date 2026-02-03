b5643a5757de77b0ea7e0cc28235e3d1d90d862b
tokotoko666 <cappuccino969@gmail.com>
2026-02-03 20:54:33 +0900

    09-29_GET comments_200 OK：ArticleCommentService-create のテストを書く

--- src/test/java/com/example/blog/service/article/ArticleCommentServiceTest.java
+++ src/test/java/com/example/blog/service/article/ArticleCommentServiceTest.java
@@ -3,6 +3,7 @@ package com.example.blog.service.article;
 import com.example.blog.config.MybatisDefaultDataSourceTest;
 import com.example.blog.config.PasswordEncoderConfig;
 import com.example.blog.service.DateTimeService;
+import com.example.blog.service.exception.ResourceNotFoundException;
 import com.example.blog.service.user.UserService;
 import com.example.blog.util.TestDateTimeUtil;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @MybatisDefaultDataSourceTest
@@ -50,5 +52,21 @@ class ArticleCommentServiceTest {
                 .ignoringFields("password")
                 .isEqualTo(commentAuthor);
         assertThat(actual.getCreatedAt()).isEqualTo(expectedCurrentDateTime);
+    }
+
+    @Test
+    @DisplayName("create: 指定された記事IDが存在しないとき、ResourceNotFoundException を投げる")
+    void create_ArticleDoesNotExist() {
+        // ## Arrange ##
+        var expectedCurrentDateTime = TestDateTimeUtil.of(2020, 1, 2, 10, 20, 30);
+        when(mockDateTimeService.now()).thenReturn(expectedCurrentDateTime);
+
+        var commentAuthor = userService.register("test_username2", "test_password");
+
+        // ## Act ##
+        // ## Assert ##
+        assertThrows(ResourceNotFoundException.class, () -> {
+            cut.create(commentAuthor.getId(), 0, "test_comment_body");
+        });
     }
 }
