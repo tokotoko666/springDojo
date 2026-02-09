6b44acce2144a7340c42f8d800fe50bf40932740
tokotoko666 <cappuccino969@gmail.com>
2026-02-09 22:35:45 +0900

    10-12_Presigned URL 生成コードをAWS公式ドキュメントから移植する

--- src/main/java/com/example/blog/service/StorageService.java
+++ src/main/java/com/example/blog/service/StorageService.java
@@ -1,11 +1,47 @@
 package com.example.blog.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
+import java.time.Duration;
+import java.util.Map;
+
+@Slf4j
 @Service
 public class StorageService {
 
     public String createUploadURL(String fileName, String contentType, Long contentLength) {
-        return "dummy";
+        return createPresignedUrl("test-bucket", "test-key", Map.of());
     }
+
+    // ref. https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/examples-s3-presign.html#put-presigned-object-part1
+    /* Create a presigned URL to use in a subsequent PUT request */
+    public String createPresignedUrl(String bucketName, String keyName, Map<String, String> metadata) {
+        try (S3Presigner presigner = S3Presigner.create()) {
+
+            PutObjectRequest objectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(keyName)
+                    .metadata(metadata)
+                    .build();
+
+            PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(10))  // The URL expires in 10 minutes.
+                    .putObjectRequest(objectRequest)
+                    .build();
+
+
+            PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(presignRequest);
+            String myURL = presignedRequest.url().toString();
+            log.info("Presigned URL to upload a file to: [{}]", myURL);
+            log.info("HTTP method: [{}]", presignedRequest.httpRequest().method());
+
+            return presignedRequest.url().toExternalForm();
+        }
+    }
+
 }
