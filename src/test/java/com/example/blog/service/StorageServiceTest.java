8ce2faad93652efde9f8ff3cfebea72781124f6d
tokotoko666 <cappuccino969@gmail.com>
2026-02-09 22:20:05 +0900

    10-10_合成アノテーション @SpringJUnitConfig を使うようにする

--- src/test/java/com/example/blog/service/StorageServiceTest.java
+++ src/test/java/com/example/blog/service/StorageServiceTest.java
@@ -1,15 +1,12 @@
 package com.example.blog.service;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = StorageService.class)
+@SpringJUnitConfig(classes = StorageService.class)
 class StorageServiceTest {
 
     @Autowired
