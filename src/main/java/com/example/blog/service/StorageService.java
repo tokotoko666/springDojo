959462e7a22636611c1e3c64a73f84c9f17caa99
tokotoko666 <cappuccino969@gmail.com>
2026-02-13 21:10:24 +0900

    10-18_StorageService初期化時の1回だけS3Presignerを生成するようにする

--- src/main/java/com/example/blog/service/StorageService.java
+++ src/main/java/com/example/blog/service/StorageService.java
@@ -1,7 +1,6 @@
 package com.example.blog.service;
 
 import com.example.blog.config.S3Properties;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -19,10 +18,21 @@ import java.util.Map;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class StorageService {
 
     private final S3Properties s3Properties;
+    private final S3Presigner s3Presigner;
+
+    public StorageService(S3Properties s3Properties) {
+        this.s3Properties = s3Properties;
+        this.s3Presigner = S3Presigner
+                .builder()
+                .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
+                .endpointOverride(URI.create(s3Properties.endpoint()))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(s3Properties.accessKey(), s3Properties.secretKey())))
+                .region(Region.of(s3Properties.region()))
+                .build();
+    }
 
     public String createUploadURL(String fileName, String contentType, Long contentLength) {
         return createPresignedUrl(s3Properties.bucket().profileImages(), "test-key", Map.of());
@@ -31,34 +41,23 @@ public class StorageService {
     // ref. https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/examples-s3-presign.html#put-presigned-object-part1
     /* Create a presigned URL to use in a subsequent PUT request */
     private String createPresignedUrl(String bucketName, String keyName, Map<String, String> metadata) {
-        var builder = S3Presigner
-                .builder()
-                .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
-                .endpointOverride(URI.create(s3Properties.endpoint()))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(s3Properties.accessKey(), s3Properties.secretKey())))
-                .region(Region.of(s3Properties.region()));
 
-        try (S3Presigner presigner = builder.build()) {
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(keyName)
+                .metadata(metadata)
+                .build();
 
-            PutObjectRequest objectRequest = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(keyName)
-                    .metadata(metadata)
-                    .build();
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))  // The URL expires in 10 minutes.
+                .putObjectRequest(objectRequest)
+                .build();
 
-            PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
-                    .signatureDuration(Duration.ofMinutes(10))  // The URL expires in 10 minutes.
-                    .putObjectRequest(objectRequest)
-                    .build();
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
+        String myURL = presignedRequest.url().toString();
+        log.info("Presigned URL to upload a file to: [{}]", myURL);
+        log.info("HTTP method: [{}]", presignedRequest.httpRequest().method());
 
-
-            PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(presignRequest);
-            String myURL = presignedRequest.url().toString();
-            log.info("Presigned URL to upload a file to: [{}]", myURL);
-            log.info("HTTP method: [{}]", presignedRequest.httpRequest().method());
-
-            return presignedRequest.url().toExternalForm();
-        }
+        return presignedRequest.url().toExternalForm();
     }
-
 }
